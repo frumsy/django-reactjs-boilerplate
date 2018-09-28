c26a6bfc12cc8ad41ace04b3dde9995976ecf5b4
frumsy <liambritt3@gmail.com>
2018-09-28 14:02:04 -0400

    change boilerplate components

---
+++ app/modules/header/components/HeaderComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export default class HeaderComponent extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <h1>ThisIsTheHeaderComponent</h1>
+            </div>
+        )
+    }
+}
--- app/static/js/App.jsx
+++ app/static/js/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-import LoginComponent from '../../modules/login/components/LoginComponent'
+import HeaderComponent from '../../modules/header/components/HeaderComponent'
 
 export default class App extends React.Component{
     render(){
         return(
             <div>
-              <LoginComponent />
+              <HeaderComponent />
             </div>
         )
     }
